e670dc8fcafe802030226f158c6e5570851b228f
Anubhav <lucimgstar007@gmail.com>
2024-04-21 14:23:55 +0530

    Update apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URI = "http://localhost:8800/api";
-const API_URI = "http://localhost:8080";
+const API_URI = "https://collaborators-8cwo.onrender.com";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api" });
 
