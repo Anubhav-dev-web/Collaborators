98c95775bc099ad9e44bf75bb33a948ef931f85f
Anubhav <lucimgstar007@gmail.com>
2024-04-21 15:52:55 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -82,9 +82,9 @@ const Sidebar = () => {
     <div className="w-full  h-full flex flex-col gap-6 p-5">
       <h1 className="flex gap-1 items-center">
         <p className="bg-blue-600 p-2 rounded-full">
-          <MdOutlineAddTask className="text-white text-2xl font-black" />
+          <img src="logo.svg" alt="logo" className="w-10 h-10" />
         </p>
-        <span className="text-2xl font-bold text-black">TaskMe</span>
+       <span className="text-2xl font-bold text-black">Collaborator</span>
       </h1>
 
       <div className="flex-1 flex flex-col gap-y-5 py-8">
